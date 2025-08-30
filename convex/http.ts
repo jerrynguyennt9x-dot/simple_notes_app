cc8d5cb6ea72f3aa402ac64892f565bbb322112b
jerrynguyennt9x-dot <jerry.nguyen.nt9x@gmail.com>
2025-08-30 16:56:37 +0700

    feat: refactor HTTP routing by removing unused files and adding a new hello route

--- convex/http.ts
+++
@@ -1,8 +0,0 @@
-import { httpAction } from "./_generated/server";
-
-// HTTP action đơn giản để test API
-export const hello = httpAction(async ({ runQuery, runMutation }, request) => {
-  return new Response("Hello from Convex API!", {
-    status: 200,
-  });
-});
